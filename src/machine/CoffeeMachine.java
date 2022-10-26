eeaf96f962c4a78da9caf3448b4d7c309eb18202
Colton C Ward <colton1ward@gmail.com>
2022-10-26 08:13:27 -0500

    Finished stage 3

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -7,19 +7,39 @@ public class CoffeeMachine {
   private static final int ML_WATER_PER_CUP = 200;
   private static final int ML_MILK_PER_CUP = 50;
   private static final int G_COFFEE_PER_CUP = 15;
+  private static final Scanner scanner = new Scanner(System.in);
+
+  private static int calculateMaxCups(int water, int milk, int coffee) {
+    int maxWater = water / ML_WATER_PER_CUP;
+    int maxMilk = milk / ML_MILK_PER_CUP;
+    int maxCoffee = coffee / G_COFFEE_PER_CUP;
+    int max = maxWater;
+    max = maxMilk < max ? maxMilk : max;
+    max = maxCoffee < max ? maxCoffee : max;
+    return max;
+  }
+
+  private static int inInt(String request) {
+    if (request != null) {
+      System.out.printf("%s:%n", request);
+    }
+    return scanner.nextInt();
+  }
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Write how many cups of coffee you need:");
-    int numCups = scanner.nextInt();
+    int water = inInt("Write how many ml of water the coffee machine has");
+    int milk = inInt("Write how many ml of milk the coffee machine has");
+    int coffee = inInt("Write how many grams of coffee beans the coffee machine has");
+    int numCups = inInt("Write how many cups of coffee you need");
 
-    int mlWater = numCups * ML_WATER_PER_CUP;
-    int mlMilk = numCups * ML_MILK_PER_CUP;
-    int gCoffeeBeans = numCups * G_COFFEE_PER_CUP;
-
-    System.out.printf("For %d cups of coffee you will need:%n", numCups);
-    System.out.printf("%d ml of water%n", mlWater);
-    System.out.printf("%d ml of milk%n", mlMilk);
-    System.out.printf("%d g of coffee beans%n", gCoffeeBeans);
+    int maxCups = calculateMaxCups(water, milk, coffee);
+    if (numCups == maxCups) {
+      System.out.println("Yes, I can make that amount of coffee");
+    } else if (numCups < maxCups) {
+      System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)%n",
+          maxCups - numCups);
+    } else {
+      System.out.printf("No, I can make only %d cup(s) of coffee%n", maxCups);
+    }
   }
 }
