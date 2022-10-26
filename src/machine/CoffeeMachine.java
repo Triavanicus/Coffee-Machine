eae110c255bb51acff2a715cdefed8e7f39381dc
Colton C Ward <colton1ward@gmail.com>
2022-10-26 07:42:21 -0500

    Finished stage 2

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -1,14 +1,25 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
 
+  private static final int ML_WATER_PER_CUP = 200;
+  private static final int ML_MILK_PER_CUP = 50;
+  private static final int G_COFFEE_PER_CUP = 15;
+
   public static void main(String[] args) {
-    System.out.println("Starting to make a coffee");
-    System.out.println("Grinding coffee beans");
-    System.out.println("Boiling water");
-    System.out.println("Mixing boiled water with crushed coffee beans");
-    System.out.println("Pouring coffee into the cup");
-    System.out.println("Pouring some milk into the cup");
-    System.out.println("Coffee is ready!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Write how many cups of coffee you need:");
+    int numCups = scanner.nextInt();
+
+    int mlWater = numCups * ML_WATER_PER_CUP;
+    int mlMilk = numCups * ML_MILK_PER_CUP;
+    int gCoffeeBeans = numCups * G_COFFEE_PER_CUP;
+
+    System.out.printf("For %d cups of coffee you will need:%n", numCups);
+    System.out.printf("%d ml of water%n", mlWater);
+    System.out.printf("%d ml of milk%n", mlMilk);
+    System.out.printf("%d g of coffee beans%n", gCoffeeBeans);
   }
 }
